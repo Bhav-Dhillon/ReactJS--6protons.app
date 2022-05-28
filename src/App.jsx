10630b365d60975fbca34362800edf0f54742e0f
Bhav-Dhillon <b.dhillon913@gmail.com>
2022-05-28 10:56:53 -0700

    add hover and transform effect to lesson cards

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
   const [moved, setMoved] = useState(false);
   const [txtAn, setTxtAn] = useState(false);
 
-  const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(true);
 
   // useFrame((state) => {
   //   // state.camera.position.lerp([0, 0, z], )
@@ -130,6 +130,44 @@ function Stars(props)
 
 function LessonSelection()
 {
+  useEffect(() => {
+    const frame = document.querySelector('.frame')
+    const card = document.querySelector('.lessonSelection--container')
+    const light = document.querySelector('.light')
+
+    let { x, y, width, height } = frame.getBoundingClientRect()
+
+    function mouseMove(e)
+    { 
+        const left = e.clientX - x
+        const top = e.clientY - y
+        const centerX = left - width / 2
+        const centerY = top - height / 2
+        const d = Math.sqrt(centerX ** 2 + centerY ** 2)
+
+        card.style.boxShadow = `
+        ${-centerX / 5}px ${-centerY / 10}px 10px rgba(0, 0, 0, 0.2)`
+
+        card.style.transform = `
+        rotate3d(${-centerY / 100}, ${centerX / 100}, 0, ${d / 8}deg)`
+
+        light.style.backgroundImage = `
+        radial-gradient(circle at ${left}px ${top}px, #00000040, #ffffff00, #ffffff99)`
+    }
+
+    frame.addEventListener('mouseenter', () =>
+    {
+        frame.addEventListener('mousemove', mouseMove)
+    })
+
+    frame.addEventListener('mouseleave', () =>
+    {
+        frame.removeEventListener('mousemove', mouseMove)
+        card.style.boxShadow = ''
+        card.style.transform = ''
+        light.style.backgroundImage = ''
+    })
+  }) 
   return (
     <>
       <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }}>
@@ -137,7 +175,7 @@ function LessonSelection()
       </div>
       <div className='lessonSelection-wrapper'>
         <div className='frame'>
-          <section className='lessonSelection--container'></section>
+          <section className='lessonSelection--container'><div className='light'></div></section>
         </div>
         <div className='frame'>
           <section className='lessonSelection--container'></section>
@@ -154,9 +192,6 @@ function LessonSelection()
 
 function LessonSelectionProtoype()
 {
-  // const ref = useRef();
-  // if (!ref.current) return
-
   useEffect(() => {
     const frame = document.querySelector('.frame')
     const card = document.querySelector('.card')
@@ -194,16 +229,34 @@ function LessonSelectionProtoype()
         card.style.transform = ''
         light.style.backgroundImage = ''
     })
-
   }) 
+
+  function Card() 
+  {
+    return (
+      <div className="frame">
+        <div className="card">
+          <div className="light"></div>
+        </div>
+      </div>
+    )
+  }
   
 
 
   return (
-    <div className="frame">
-      <div className="card">
-        <div className="light"></div>
+    <>
+      {/* <div sty le={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }}>
+      </div> */}
+      <div className='lessonSelection-wrapper'>
+        <h1 className='lessonSelection--title'>Please select a lesson.</h1>
+        <div className='card--wrapper'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
-    </div>
+    </>
+
   )
 }
